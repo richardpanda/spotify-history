7a2169293211684c5b909d832230af83128ca5be
Richard Ho <richardpanda1993@gmail.com>
2017-12-02 17:22:34 -0800

    Get list of recently played tracks

--- cmd/main.go
+++ cmd/main.go
@@ -8,77 +8,70 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"time"
+	"text/tabwriter"
 )
-
-type Song struct {
-	Artists []Artist `json:"artists"`
-	Name    string   `json:"name"`
-}
 
 type Artist struct {
 	Name string `json:"name"`
 }
 
 type PlayHistory struct {
-	Song Song `json:"item"`
+	Track Track `json:"track"`
+}
+
+type ResponseBody struct {
+	Items []PlayHistory `json:"items"`
+}
+
+type Track struct {
+	Artists []Artist `json:"artists"`
+	Name    string   `json:"name"`
 }
 
 func main() {
 	token := os.Getenv("OAUTH_TOKEN")
 	client := &http.Client{}
-	var currentSong, previousSong Song
 
-	for {
-		func() {
-			defer time.Sleep(1 * time.Minute)
-
-			req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player", nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			req.Header.Add("Accept", "application/json")
-			req.Header.Add("Content-Type", "application/json")
-			req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-
-			resp, err := client.Do(req)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode == 400 {
-				return
-			}
-
-			body, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if resp.StatusCode == 401 {
-				log.Fatal(string(body))
-			}
-
-			var playHistory PlayHistory
-			err = json.Unmarshal(body, &playHistory)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			currentSong = playHistory.Song
-			if previousSong.Name == currentSong.Name {
-				return
-			}
-
-			previousSong = currentSong
-			artistNames := make([]string, len(currentSong.Artists))
-			for i, artist := range currentSong.Artists {
-				artistNames[i] = artist.Name
-			}
-
-			fmt.Printf("%s - %s\n", currentSong.Name, strings.Join(artistNames, ", "))
-		}()
+	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player/recently-played", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode == 401 {
+		log.Fatal(string(body))
+	}
+
+	var responseBody ResponseBody
+	err = json.Unmarshal(body, &responseBody)
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+	fmt.Println("\n")
+	fmt.Fprintln(w, "Title\tArtist\t")
+	for _, playHistory := range responseBody.Items {
+		trackName := playHistory.Track.Name
+		artists := playHistory.Track.Artists
+
+		artistNames := make([]string, len(artists))
+		for i, artist := range artists {
+			artistNames[i] = artist.Name
+		}
+
+		fmt.Fprintf(w, "%s\t%s\n", trackName, strings.Join(artistNames, ", "))
+	}
+	w.Flush()
 }
