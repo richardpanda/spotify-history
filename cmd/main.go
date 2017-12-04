7de2f69b03f4cbbfebe270cee8c24f0c049260fa
Richard Ho <richardpanda1993@gmail.com>
2017-12-03 19:07:58 -0800

    Save auth and get recently played tracks

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/richardpanda/spotify-history/spotify"
 )
-
-type Artist struct {
-	Name string `json:"name"`
-}
-
-type PlayHistory struct {
-	Track Track `json:"track"`
-}
-
-type ResponseBody struct {
-	Items []PlayHistory `json:"items"`
-}
-
-type Track struct {
-	Artists []Artist `json:"artists"`
-	Name    string   `json:"name"`
-}
 
 func openAuthWindow(clientID, redirectURI string) error {
 	responseType := "code"
@@ -45,85 +32,118 @@ func openAuthWindow(clientID, redirectURI string) error {
 	return nil
 }
 
+func printTracks(tracks []spotify.Track) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
+	fmt.Println("\n")
+	fmt.Fprintln(w, "Title\tArtist\t")
+	for _, track := range tracks {
+		artistNames := make([]string, len(track.Artists))
+		for i, artist := range track.Artists {
+			artistNames[i] = artist.Name
+		}
+
+		fmt.Fprintf(w, "%s\t%s\n", track.Name, strings.Join(artistNames, ", "))
+	}
+	w.Flush()
+}
+
+func saveAuth(auth *spotify.Auth) error {
+	b, err := json.Marshal(auth)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile("auth.json", b, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	clientID := "788fcd8f8c484f6e9df10ffab16c8844"
 	clientSecret := os.Getenv("CLIENT_SECRET")
 	redirectURI := "http://localhost:7000/"
 
-	err := openAuthWindow(clientID, redirectURI)
+	_, err := os.Stat("auth.json")
+	if err != nil {
+		err = openAuthWindow(clientID, redirectURI)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		})
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			err := r.ParseForm()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			params := spotify.AuthRequestParams{
+				ClientID:     clientID,
+				ClientSecret: clientSecret,
+				Code:         r.Form.Get("code"),
+				RedirectURI:  redirectURI,
+			}
+
+			auth, err := spotify.FetchAuth(params)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = saveAuth(auth)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			tracks, err := spotify.RecentlyPlayedTracks(auth.AccessToken)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			printTracks(tracks)
+			os.Exit(0)
+		})
+
+		log.Fatal(http.ListenAndServe(":7000", nil))
+	}
+
+	fileBytes, err := ioutil.ReadFile("auth.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-	})
+	auth := &spotify.Auth{}
+	err = json.Unmarshal(fileBytes, auth)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := r.ParseForm()
-		if err != nil {
-			log.Fatal(err)
-		}
-
+	tracks, err := spotify.RecentlyPlayedTracks(auth.AccessToken)
+	if err != nil {
 		params := spotify.AuthRequestParams{
 			ClientID:     clientID,
 			ClientSecret: clientSecret,
-			Code:         r.Form.Get("code"),
-			RedirectURI:  redirectURI,
+			RefreshToken: auth.RefreshToken,
 		}
-
-		auth, err := spotify.FetchAuth(params)
+		newAuth, err := spotify.FetchAuth(params)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%+v\n", auth)
-	})
+		auth.AccessToken = newAuth.AccessToken
+		err = saveAuth(auth)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	log.Fatal(http.ListenAndServe(":7000", nil))
+		tracks, err = spotify.RecentlyPlayedTracks(auth.AccessToken)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-	// token := os.Getenv("OAUTH_TOKEN")
-	// client := &http.Client{}
-
-	// req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player/recently-played", nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// req.Header.Add("Accept", "application/json")
-	// req.Header.Add("Content-Type", "application/json")
-	// req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
-
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer resp.Body.Close()
-
-	// body, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if resp.StatusCode == 401 {
-	// 	log.Fatal(string(body))
-	// }
-
-	// var responseBody ResponseBody
-	// err = json.Unmarshal(body, &responseBody)
-
-	// w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
-	// fmt.Println("\n")
-	// fmt.Fprintln(w, "Title\tArtist\t")
-	// for _, playHistory := range responseBody.Items {
-	// 	trackName := playHistory.Track.Name
-	// 	artists := playHistory.Track.Artists
-
-	// 	artistNames := make([]string, len(artists))
-	// 	for i, artist := range artists {
-	// 		artistNames[i] = artist.Name
-	// 	}
-
-	// 	fmt.Fprintf(w, "%s\t%s\n", trackName, strings.Join(artistNames, ", "))
-	// }
-	// w.Flush()
+	printTracks(tracks)
 }
--- spotify/spotify.go
+++ spotify/spotify.go
@@ -3,10 +3,18 @@ package spotify
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 )
+
+type Artist struct {
+	Name string `json:"name"`
+}
 
 type Auth struct {
 	AccessToken  string `json:"access_token"`
@@ -18,6 +26,19 @@ type AuthRequestParams struct {
 	Code                   string
 	RedirectURI            string
 	RefreshToken           string
+}
+
+type PlayHistory struct {
+	Track Track `json:"track"`
+}
+
+type Track struct {
+	Artists []Artist `json:"artists"`
+	Name    string   `json:"name"`
+}
+
+type TracksResponseBody struct {
+	Items []PlayHistory `json:"items"`
 }
 
 func FetchAuth(params AuthRequestParams) (*Auth, error) {
@@ -57,4 +78,42 @@ func FetchAuth(params AuthRequestParams) (*Auth, error) {
 		return nil, err
 	}
 	return &auth, nil
+}
+
+func RecentlyPlayedTracks(accessToken string) ([]Track, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.spotify.com/v1/me/player/recently-played", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 401 {
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(string(b))
+	}
+
+	respBody := &TracksResponseBody{}
+	err = json.NewDecoder(resp.Body).Decode(respBody)
+	if err != nil {
+		return nil, err
+	}
+
+	tracks := make([]Track, len(respBody.Items))
+	for i, item := range respBody.Items {
+		tracks[i] = item.Track
+	}
+	return tracks, nil
 }
